29530ef8bae718a4a804d53b871d1fb75d18441e
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-15 22:35:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+  people.map do |person|
+    badge_maker(person)
+  end
+end
