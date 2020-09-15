6762b0e279a6c67b25c039f2090384118a5135aa
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-15 22:47:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+  people.map do |person|
+    badge_maker(person)
+  end
+end
+
+def assign_rooms(people)
+  people.map.with_index(1) do |person, index|
+    #binding.pry
+    "Hello, #{person}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(people)
+  # binding.pry
+  batch_badge_creator(people).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(people).each do |assignment|
+    puts assignment
+  end
+end
